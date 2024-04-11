ced3d73d41fd9cfb35d6d7f4e88d68a6b9b91a63
rynplynch <rynplynch@gmail.com>
2024-04-10 23:58:58 -0700

    trying boat mechanics

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -20,7 +20,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        
+
         transform.position = player.transform.position + offset;
 
         transform.LookAt(playerRB.velocity + player.transform.position);
